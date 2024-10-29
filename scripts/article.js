fb7074018bcdd18c689acbe07a2980f219ac2d11
b-alvd <basilealeveque@gmail.com>
2024-10-29 12:25:10 +0100

    add page inspirations & section inspirations + article details & responsive

---
+++ scripts/article.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const URL_PARAMS = new URLSearchParams(window.location.search);
+    const ARTICLE_ID = URL_PARAMS.get("id");
+
+    fetch(`./scripts/articles.json`)
+    .then(response => response.json())
+    .then(articles => {
+        const ARTICLE = articles.find(article => article.id == ARTICLE_ID);
+
+        if (ARTICLE) {
+            document.querySelector("#article_date").textContent = ARTICLE.date;
+            document.querySelector("#article_readtime").textContent = ARTICLE.readTime;
+            document.querySelector("#article_title").textContent = ARTICLE.title;
+            document.querySelector("#article_image").src = ARTICLE.imgSrc;
+            document.querySelector("#article_content").textContent = ARTICLE.content;
+        } else {
+            document.getElementById("article_details").innerHTML = "<p>Article introuvable</p>";
+        }
+    })
+    .catch(error => console.error("Erreur lors du chargement de l'article: ", error));
+})
---
+++ scripts/blog.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const POSTS_CONTAINER = document.querySelector(".posts_container");
+
+    fetch("./scripts/articles.json")
+    .then(response => response.json())
+    .then(articles => {
+        articles.forEach(article => {
+            const ARTICLE_ELEMENT = document.createElement("div");
+            ARTICLE_ELEMENT.classList.add("article");
+
+            ARTICLE_ELEMENT.innerHTML = `
+                <img src="${article.imgSrc}" alt="${article.title}">
+                <div class="content">
+                    <div class="date_readtime">
+                        <p>${article.date}</p>
+                        <p>${article.readTime}</p>
+                    </div>
+                    <h3>${article.title}</h3>
+                    <p>${article.content.slice(0, 100)}...</p>
+                    <a href="article.html?id=${article.id}">Voir plus</a>
+                </div>
+            `;
+            POSTS_CONTAINER.appendChild(ARTICLE_ELEMENT);
+        })
+    })
+    .catch(error => console.error("Erreur lors du chargement des articles: ", error));
+})
